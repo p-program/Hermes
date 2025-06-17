d0120bf9df3043ba88fa155eaf38f2a7cad171e6
Zeusro <unccav@gmai.com>
2025-06-17 19:32:01 +0800

    Tue Jun 17 19:32:01 CST 2025

--- function/web/translate/module.go
+++ function/web/translate/module.go
@@ -31,7 +31,7 @@ func NewDeepSeekTranslator(apiKey string) DeepSeekTranslator {
 func (d DeepSeekTranslator) Translate(source string, targets []string) (cost time.Duration, output string, err error) {
 	// 猜语言这个事情交给ai算了
 	// country:=
-	prompt := fmt.Sprintf("使用最简短的语言将下述文字翻译成多国语言（%s）：%s", strings.Join(targets, ","), source)
+	prompt := fmt.Sprintf("使用最简短的纯文本（不包含markdown）将下述文字翻译成多国语言（%s）：%s", strings.Join(targets, ","), source)
 	start := time.Now()
 	defer func() {
 		cost = time.Since(start)
@@ -41,7 +41,6 @@ func (d DeepSeekTranslator) Translate(source string, targets []string) (cost tim
 	w.Add(1)
 	go func() {
 		defer w.Done()
-		// prompt := "使用最简短的语言将下述文字翻译成多国语言（英文，日语）：我想学习Go语言。"
 		response, err := web.CallDeepSeek(prompt, d.ApiKey)
 		if err != nil {
 			fmt.Println("调用失败：", err)
